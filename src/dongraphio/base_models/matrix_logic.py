1572b952ae80f2ecd1d1ddf8fd222da641ebe379
Danila <63115678+DDonnyy@users.noreply.github.com>
2024-05-19 18:10:21 +0300

    More TODOs

--- src/dongraphio/base_models/matrix_logic.py
+++ src/dongraphio/base_models/matrix_logic.py
@@ -25,11 +25,14 @@ class BuildsMatrixer(BaseModel):
     def get_adjacency_matrix(self) -> pd.DataFrame:
         self.gdf_from.to_crs(self.city_crs, inplace=True)
         self.gdf_to.to_crs(self.city_crs, inplace=True)
+
+        # TODO check if selected GraphType exists in graph
         mobility_sub_graph = get_subgraph(
             self.nx_intermodal_graph,
             "type",
             [t.value for t in sum([t.edges for t in self.graph_type], [])],
         )
+
 
         if mobility_sub_graph.is_multigraph() & mobility_sub_graph.is_directed():
             mobility_sub_graph = convert_multidigraph_to_digraph(mobility_sub_graph, self.weight)
@@ -38,14 +41,19 @@ class BuildsMatrixer(BaseModel):
 
         graph_gdf = nx_to_gdf(nx.MultiDiGraph(mobility_sub_graph), nodes=True)
 
+        # TODO use gdf.index.name to determine loc column name, could be different from 'index'
         from_ = gpd.sjoin_nearest(self.gdf_from, graph_gdf)
+        from_ = from_.reset_index().drop_duplicates(subset="index", keep="first").set_index('index')
+
         to_ = gpd.sjoin_nearest(self.gdf_to, graph_gdf)
+        to_ = to_.reset_index().drop_duplicates(subset="index", keep="first").set_index("index")
+
         from_index = from_["index_right"]
         to_index = to_["index_right"]
         distance_matrix = pd.DataFrame(float(0), index=from_index, columns=to_index)
         from_index = list(set(from_index))
         to_index = list(set(to_index))
-        logger.debug("Calculating distances from buildings to services ...")
+        logger.debug("Calculating distances ...")
 
         distance_matrix_result = get_dist_matrix(
             mobility_sub_graph,
